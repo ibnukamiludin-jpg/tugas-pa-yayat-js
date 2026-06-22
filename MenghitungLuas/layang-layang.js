f7ce1f29c976c368cdce8aa7fec617b1efbae4d8
ibnukamiludin-jpg <ibnukamiludin@gmail.com>
2026-06-22 21:16:16 +0700

    Add files via upload

---
+++ MenghitungLuas/layang-layang.js
@@ -0,0 +1,14 @@
+const prompt = require("prompt-sync")({ sigint: true })
+let diagonal1 = Number(prompt('Masukan diagonal 1: '))
+let diagonal2 = Number(prompt('Masukan diagonal 2: '))
+let sisi1 = Number(prompt('Masukan sisi 1: '))
+let sisi2 = Number(prompt('Masukan sisi 2: '))
+let luas = diagonal1 * diagonal2 / 2
+let keliling = 2 * (sisi1 + sisi2)
+
+console.log("========================")
+console.log("         HASIL          ")
+console.log("========================")
+
+console.log(`Luas layang-layang\t: ${luas.toFixed(2)} cm2`)
+console.log(`Keliling layang-layang\t: ${keliling.toFixed(2)} cm`)
